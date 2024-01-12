c9913c3f4af7d81df217f660c0ccd0e2c004d3c3
David Levy <david.g.levy@gmail.com>
2024-01-13 08:18:21 +1100

    setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     description='Vectara Skunk Client',
     long_description=get_long_desc(),
     long_description_content_type='text/markdown',
-    version='0.4.6',
+    version='0.4.7',
     author='David Levy',
     author_email='david.g.levy@gmail.com',
     url='https://github.com/davidglevy/vectara-skunk-client',
